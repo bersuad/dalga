75b35e2a2a951e44ecda37b842c1c96e2f6dc175
bersuad <bersuadane@gmail.com>
2021-11-19 11:43:08 +0300

    dala comming soon

---
+++ application/views/included/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Dalga Leather</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Dalga Leather" />
+	<meta name="keywords" content="Dalga Leather, Dalga Leather Ethiopia" />
+	<meta name="author" content="Dalga Leather" />
+	<link rel="shortcut icon" type="image/png" href="<?php echo base_url() ?>assets/images/dalaga_circle.png" />
+
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	
+	<link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
+	
+	<link rel="stylesheet" href="<?php echo base_url('assets/') ?>css/animate.css">
+	<link rel="stylesheet" href="<?php echo base_url('assets/') ?>css/icomoon.css">
+	<link rel="stylesheet" href="<?php echo base_url('assets/') ?>css/bootstrap.css">
+	<link rel="stylesheet" href="<?php echo base_url('assets/') ?>css/style.css">
+
+	<script src="<?php echo base_url('assets/') ?>js/modernizr-2.6.2.min.js"></script>
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
---
+++ application/views/pages/index.php
@@ -0,0 +1,46 @@
+<div id="page">
+
+	<div id="fh5co-container" class="js-fullheight">
+		<div class="countdown-wrap js-fullheight">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<div class="display-t js-fullheight">
+						<div class="display-tc animate-box">
+							<nav class="fh5co-nav" role="navigation">
+								<div id="fh5co-logo"><a href="<?php echo base_url('/'); ?>">Coming Soon<strong>.</strong></a></div>
+							</nav>
+							<h1>We Are Coming Soon!</h1>
+							<h2><img src="<?php echo base_url() ?>assets/images/dalga_listed.png" style="width:auto; height: 60px;"/></h2>
+							<h2>Leather</h2>
+							<div class="simply-countdown simply-countdown-one"></div>
+							<div class="row">
+								<div class="col-md-12 desc">
+									<h2>Our webiste is opening soon. <br> Please register to notify you when it's ready!</h2>
+									<form class="form-inline" id="fh5co-header-subscribe">
+										<div class="col-md-12 col-md-offset-0">
+											<div class="form-group">
+												<input type="text" class="form-control" id="email" placeholder="Get notify by email">
+												<button type="submit" class="btn btn-primary">Subscribe</button>
+											</div>
+										</div>
+									</form>
+									<ul class="fh5co-social-icons">
+										<li><a href="#"><i class="icon-twitter-with-circle"></i></a></li>
+										<li><a href="#"><i class="icon-facebook-with-circle"></i></a></li>
+										<li><a href="#"><i class="icon-linkedin-with-circle"></i></a></li>
+										<li><a href="#"><i class="icon-dribbble-with-circle"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="bg-cover js-fullheight" style="background-image:url(<?php echo base_url('assets/') ?>images/bg_dalga.jpg);">
+			<div>
+				<img src="<?php echo base_url() ?>assets/images/dalga_listed.png" style="width:400px; height: auto; position: absolute; bottom:0; left: 0; margin-bottom: 170px; margin-left: 20px;"/>
+			</div>
+		</div>
+	</div>
+	</div>
